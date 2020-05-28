53e227d42f3b9f217f8767eccc9efabe9f34a30f
mattbertolini <mattbertolini@users.noreply.github.com>
2020-05-27 22:39:18 -0400

    Add missing webflux docs test.

--- docs/src/test/java/com/mattbertolini/spring/web/bind/docs/webflux/WebFluxDocsJavaConfigIntegrationTest.java
+++ docs/src/test/java/com/mattbertolini/spring/web/bind/docs/webflux/WebFluxDocsJavaConfigIntegrationTest.java
@@ -16,8 +16,34 @@
 
 package com.mattbertolini.spring.web.bind.docs.webflux;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.web.reactive.function.BodyInserters.fromFormData;
 
 @SpringJUnitWebConfig(classes = {ExampleContext.class})
 class WebFluxDocsJavaConfigIntegrationTest {
+    private WebTestClient webTestClient;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext) {
+        webTestClient = WebTestClient.bindToApplicationContext(webApplicationContext).build();
+    }
+
+    @Test
+    void name() {
+        webTestClient.post()
+            .uri("/example/42?different_name=different_value")
+            .accept(MediaType.TEXT_PLAIN)
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .body(fromFormData("form_data_", "form_value"))
+            .header("Accept-Language", "en-US")
+            .header("X-Custom-Header", "A_Header_Value")
+            .cookie("cookie_value", "some_cookie_value")
+            .exchange().expectStatus().isOk();
+    }
 }

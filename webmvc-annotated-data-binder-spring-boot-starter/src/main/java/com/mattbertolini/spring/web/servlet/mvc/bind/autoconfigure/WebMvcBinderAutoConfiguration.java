307321fd349111605b3c00db6efc0bace4a7ea62
Matt Bertolini <mattbertolini@users.noreply.github.com>
2021-06-20 18:26:33 -0400

    Spring Boot Starter Modules (#5)
    * Skeleton for WebMVC Spring Boot auto configuration.
    
    * Add support for package scanning in webmvc auto configuration.
    
    * Add webflux auto configuration module.
    
    * Polishing
    
    * Move conditional to class level. Add dependency to platform.
    
    * Add some unit tests for auto configuration.
    
    * Add header.
    
    * Use starter dependency.
    
    * Use ObjectProvider instead of Optional.
    
    * Update Spring versions.

---
+++ webmvc-annotated-data-binder-spring-boot-starter/src/main/java/com/mattbertolini/spring/web/servlet/mvc/bind/autoconfigure/WebMvcBinderAutoConfiguration.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2019-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mattbertolini.spring.web.servlet.mvc.bind.autoconfigure;
+
+import com.mattbertolini.spring.web.servlet.mvc.bind.PropertyResolverRegistry;
+import com.mattbertolini.spring.web.servlet.mvc.bind.config.BinderConfiguration;
+import com.mattbertolini.spring.web.servlet.mvc.bind.resolver.RequestPropertyResolver;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
+
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+@Configuration(proxyBeanMethods = false)
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+@ConditionalOnMissingBean(BinderConfiguration.class)
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class WebMvcBinderAutoConfiguration {
+    private final List<String> packagesToScan = new LinkedList<>();
+    private final Set<RequestPropertyResolver> customResolvers = new LinkedHashSet<>();
+    private final Set<PropertyResolverRegistry> propertyResolverRegistries = new LinkedHashSet<>();
+    
+    public WebMvcBinderAutoConfiguration(BeanFactory beanFactory,
+                                         ObjectProvider<List<RequestPropertyResolver>> customResolvers,
+                                         ObjectProvider<List<PropertyResolverRegistry>> propertyResolverRegistries) {
+        if (AutoConfigurationPackages.has(beanFactory)) {
+            packagesToScan.addAll(AutoConfigurationPackages.get(beanFactory));
+        }
+        customResolvers.ifAvailable(this.customResolvers::addAll);
+        propertyResolverRegistries.ifAvailable(this.propertyResolverRegistries::addAll);
+    }
+
+    @Bean
+    public BinderConfiguration binderConfiguration() {
+        BinderConfiguration binderConfiguration = new BinderConfiguration();
+        packagesToScan.forEach(binderConfiguration::addPackageToScan);
+        binderConfiguration.addResolvers(customResolvers);
+        propertyResolverRegistries.forEach(binderConfiguration::addResolvers);
+        return binderConfiguration;
+    }
+}
---
+++ webmvc-annotated-data-binder-spring-boot-starter/src/test/java/com/mattbertolini/spring/web/servlet/mvc/bind/autoconfigure/WebMvcBinderAutoConfigurationTest.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2019-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mattbertolini.spring.web.servlet.mvc.bind.autoconfigure;
+
+import com.mattbertolini.spring.web.servlet.mvc.bind.config.BinderConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.runner.WebApplicationContextRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WebMvcBinderAutoConfigurationTest {
+    private final WebApplicationContextRunner contextRunner = new WebApplicationContextRunner()
+        .withConfiguration(AutoConfigurations.of(WebMvcBinderAutoConfiguration.class));
+
+    @Test
+    void autoConfiguredWithPackagesToScan() {
+        contextRunner.withUserConfiguration(EmptyConfig.class).run(context -> {
+            assertThat(context).hasSingleBean(BinderConfiguration.class);
+            BinderConfiguration binderConfiguration = context.getBean(BinderConfiguration.class);
+            assertThat(binderConfiguration.getPackagesToScan()).hasSize(1)
+                .contains(EmptyConfig.class.getPackage().getName());
+        });
+    }
+
+    @Test
+    void noPackagesToScanWhenAutoConfigurationNotEnabled() {
+        contextRunner.run(context -> {
+            assertThat(context).hasSingleBean(BinderConfiguration.class);
+            BinderConfiguration binderConfiguration = context.getBean(BinderConfiguration.class);
+            assertThat(binderConfiguration.getPackagesToScan()).isEmpty();
+        });
+    }
+
+    @Test
+    void overridesAutoConfigurationWhenBeanIsDefined() {
+        contextRunner.withUserConfiguration(OverrideBeanDefinition.class).run(context -> {
+            assertThat(context).hasSingleBean(BinderConfiguration.class);
+            assertThat(context).getBean("customBinderConfig")
+                .isEqualTo(context.getBean(BinderConfiguration.class));
+            BinderConfiguration binderConfiguration = context.getBean(BinderConfiguration.class);
+            assertThat(binderConfiguration.getPackagesToScan())
+                .containsOnly("com.mattbertolini.override");
+        });
+    }
+
+    @SpringBootApplication(proxyBeanMethods = false)
+    @ComponentScan(excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = OverrideBeanDefinition.class)
+    })
+    private static class EmptyConfig {}
+
+    @SpringBootApplication(proxyBeanMethods = false)
+    private static class OverrideBeanDefinition {
+        @Bean
+        public BinderConfiguration customBinderConfig() {
+            return new BinderConfiguration().addPackageToScan("com.mattbertolini.override");
+        }
+    }
+}

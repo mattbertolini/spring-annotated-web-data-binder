c93df21b431dd2d67d48ee466b45d69645c66246
mattbertolini <mattbertolini@users.noreply.github.com>
2022-05-12 15:42:31 -0400

    Convert webflux starter gradle file from Groovy to Kotlin.

---
+++ webflux-annotated-data-binder-spring-boot-starter/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    `java-library`
+    id("com.mattbertolini.build.maven-central-publish")
+}
+
+dependencies {
+    api(project(":spring-webflux-annotated-data-binder"))
+    api("org.springframework.boot:spring-boot-starter")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.assertj:assertj-core")
+    testImplementation("org.springframework.boot:spring-boot-test")
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            "Automatic-Module-Name" to "com.mattbertolini.spring.web.reactive.bind.autoconfigure"
+        )
+    }
+}
+
+mavenCentralPublish {
+    name.set("Spring WebFlux Annotated Data Binder Spring Boot Starter")
+    description.set("Spring Boot starter for Spring WebFlux Annotated Java Bean data binder")
+}

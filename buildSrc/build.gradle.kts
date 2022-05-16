2d09c74eed7f4d9c3e8ab7b8afc9e0302c78d560
mattbertolini <mattbertolini@users.noreply.github.com>
2022-05-16 16:49:44 -0400

    Get rid of kotlin compile warning.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     `kotlin-dsl`
 }
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
 repositories {
     mavenCentral()
 }

f91bbc39391c15f6d12f1f665c68f99994ca7378
mattbertolini <mattbertolini@users.noreply.github.com>
2023-03-27 22:49:54 -0400

    Upgrade Gradle to 8.0.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,12 +2,6 @@ plugins {
     `kotlin-dsl`
 }
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
-
 repositories {
     mavenCentral()
 }

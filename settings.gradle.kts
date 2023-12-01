8a2adfa098134a603146a5cf382e2407b53c9907
mattbertolini <mattbertolini@users.noreply.github.com>
2023-12-01 09:25:36 -0500

    Tweak project paths

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,10 +12,10 @@ dependencyResolutionManagement {
     }
 }
 
-include("spring-annotated-data-binder-core")
-include("spring-webmvc-annotated-data-binder")
-include("spring-webflux-annotated-data-binder")
-include("integration-tests")
-include("docs")
-include("webmvc-annotated-data-binder-spring-boot-starter")
-include("webflux-annotated-data-binder-spring-boot-starter")
+include(":spring-annotated-data-binder-core")
+include(":spring-webmvc-annotated-data-binder")
+include(":spring-webflux-annotated-data-binder")
+include(":integration-tests")
+include(":docs")
+include(":webmvc-annotated-data-binder-spring-boot-starter")
+include(":webflux-annotated-data-binder-spring-boot-starter")

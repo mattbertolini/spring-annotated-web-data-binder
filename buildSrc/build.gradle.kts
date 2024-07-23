4e6ef39241dc4ea58c2c8d77f6ba34da34c76349
Matt Bertolini <viper2843@gmail.com>
2024-07-22 20:01:07 -0400

    Add license plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,4 +9,5 @@ repositories {
 
 dependencies {
     implementation("net.ltgt.gradle:gradle-errorprone-plugin:4.0.1")
+    implementation("gradle.plugin.com.hierynomus.gradle.plugins:license-gradle-plugin:0.16.1")
 }
--- buildSrc/src/main/kotlin/com/mattbertolini/buildlogic/project-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/com/mattbertolini/buildlogic/project-conventions.gradle.kts
@@ -1,5 +1,9 @@
 package com.mattbertolini.buildlogic
 
+plugins {
+    id("com.mattbertolini.buildlogic.license-conventions")
+}
+
 // Configuring archive tasks to have repeatable builds
 tasks.withType<AbstractArchiveTask>().configureEach {
     isReproducibleFileOrder = true

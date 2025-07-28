ebc91812766d814ee3ced2399dc9eeac4b062a59
Matt Bertolini <viper2843@gmail.com>
2025-07-28 11:42:57 -0400

    Add version catalog to build logic

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,1 +1,8 @@
 // buildSrc settings file. Required even if this file is empty
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
